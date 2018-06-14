f5895b2715624ad524b674ad37e8529a92be7561
tjdgus3537 <tjdgus3537@naver.com>
2018-06-14 13:44:12 +0000

    programmers level1 number_array_can_be_divided

--- programmers/level1/number_array_can_be_divided.cpp
+++ programmers/level1/number_array_can_be_divided.cpp
@@ -1,48 +1,33 @@
 #include <vector>
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-long long solution(int a, int b);
+vector<int> solution(vector<int> arr, int divisor);
 
 int main() {
-    long long a, b;
-    
-    cin >> a >> b;
-
-    cout << solution(a, b) << endl;
+    //test 생략
 
     return 0;
 }
 
-long long solution(int a, int b) {
-    long long answer = 0;
-    long long sig_a, sig_b, sum_a, sum_b;
-    
-    sum_a = ((long long)abs(a) * ((long long)abs(a) + 1)) / 2;
-    sum_b = ((long long)abs(b) * ((long long)abs(b) + 1)) / 2;
-    
-    if(a > 0)
-        sig_a = 1;     
-    else if(a == 0)
-        sig_a = 0;
-    else
-        sig_a = -1;
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    priority_queue <int, vector<int>, greater<int> > pq;
 
-    if(b > 0)
-        sig_b = 1;
-    else if(b == 0)
-        sig_b = 0;
-    else
-        sig_b = -1;    
-    
-    if(sig_a * sig_b <= 0)
-        answer = sum_a * sig_a + sum_b * sig_b;
-    else
-        answer = abs(sum_a - sum_b) * sig_a + min(abs(a), abs(b)) * sig_a;
-    
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] % divisor == 0)
+            pq.push(arr[i]);
+    }
+
+    if(pq.empty())
+       pq.push(-1);
+
+    while(!pq.empty()) {
+        answer.push_back(pq.top());
+        pq.pop();
+    }
+
     return answer;
 }
