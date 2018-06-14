755d88d6b104125397346b20019a06f15ac94e9e
tjdgus3537 <tjdgus3537@naver.com>
2018-06-14 13:25:05 +0000

    programmers level number_array_can_be_divied

---
+++ programmers/level1/number_array_can_be_divided.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor);
+
+int main() {
+    //test 생략
+
+    return 0;
+}
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    return answer;
+}
